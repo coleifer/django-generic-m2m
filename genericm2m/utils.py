22f7ba7f4269fad3a74c62279bb8b4f304ba4464
Charles Leifer <coleifer@gmail.com>
2011-07-11 15:36:51 -0500

    Use 'related' for the default name when monkeypatching

--- genericm2m/utils.py
+++ genericm2m/utils.py
@@ -1,7 +1,7 @@
 from genericm2m.models import RelatedObjectsDescriptor
 
 
-def monkey_patch(model_class, name='relationships', descriptor=None):
+def monkey_patch(model_class, name='related', descriptor=None):
     rel_obj = descriptor or RelatedObjectsDescriptor()
     rel_obj.contribute_to_class(model_class, name)
     setattr(model_class, name, rel_obj)
