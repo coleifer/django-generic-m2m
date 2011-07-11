9c74ac489d70eb5bbb7bb6f6d8b5a5fbc1d4fa36
Charles Leifer <coleifer@gmail.com>
2011-07-11 14:18:44 -0500

    Adding a small utility function to make it easy to monkeypatch existing models with a generic m2m related objects descriptor

---
+++ genericm2m/utils.py
@@ -0,0 +1,8 @@
+from genericm2m.models import RelatedObjectsDescriptor
+
+
+def monkey_patch(model_class, name='relationships', descriptor=None):
+    rel_obj = descriptor or RelatedObjectsDescriptor()
+    rel_obj.contribute_to_class(model_class, name)
+    setattr(model_class, name, rel_obj)
+    return True
