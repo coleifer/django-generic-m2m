a6657a9766027e143693d74d1791bea3a21db489
Charles Leifer <coleifer@gmail.com>
2012-07-12 12:56:20 -0500

    Smarter handling of reverse lookups

--- genericm2m/models.py
+++ genericm2m/models.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models import Q
 from django.db.models.query import QuerySet
 
 
@@ -8,67 +9,68 @@ class GFKOptimizedQuerySet(QuerySet):
     def __init__(self, *args, **kwargs):
         # pop the gfk_field from the kwargs if its passed in explicitly
         self._gfk_field = kwargs.pop('gfk_field', None)
-        
+
         # call the parent class' initializer
         super(GFKOptimizedQuerySet, self).__init__(*args, **kwargs)
-    
+
     def _clone(self, *args, **kwargs):
         clone = super(GFKOptimizedQuerySet, self)._clone(*args, **kwargs)
         clone._gfk_field = self._gfk_field
         return clone
-    
+
     def get_gfk(self):
         if not self._gfk_field:
             for field in self.model._meta.virtual_fields:
                 if isinstance(field, GenericForeignKey):
                     self._gfk_field = field
                     break
-        
+
         return self._gfk_field
-    
+
     def generic_objects(self):
         clone = self._clone()
-        
+
         ctypes_and_fks = {}
-        
+
         gfk_field = self.get_gfk()
         ctype_field = '%s_id' % gfk_field.ct_field
         fk_field = gfk_field.fk_field
-        
+
         for obj in clone:
             ctype = ContentType.objects.get_for_id(getattr(obj, ctype_field))
             obj_id = getattr(obj, fk_field)
-            
+
             ctypes_and_fks.setdefault(ctype, [])
             ctypes_and_fks[ctype].append(obj_id)
-        
+
         gfk_objects = {}
         for ctype, obj_ids in ctypes_and_fks.items():
             gfk_objects[ctype.pk] = ctype.model_class()._default_manager.in_bulk(obj_ids)
-        
+
         obj_list = []
         for obj in clone:
             obj_list.append(gfk_objects[getattr(obj, ctype_field)][getattr(obj, fk_field)])
-        
+
         return obj_list
 
 
 class RelatedObjectsDescriptor(object):
-    def __init__(self, model=None, from_field='parent', to_field='object'):
+    def __init__(self, model=None, from_field='parent', to_field='object', symmetrical=False):
         self.related_model = model or RelatedObject
         self.from_field = self.get_related_model_field(from_field)
         self.to_field = self.get_related_model_field(to_field)
-    
+        self.symmetrical = symmetrical
+
     def get_related_model_field(self, field_name):
         opts = self.related_model._meta
         for virtual_field in opts.virtual_fields:
             if virtual_field.name == field_name:
                 return virtual_field
         return opts.get_field(field_name)
-    
+
     def is_gfk(self, field):
         return isinstance(field, GenericForeignKey)
-    
+
     def get_query_for_field(self, instance, field):
         if self.is_gfk(field):
             ctype = ContentType.objects.get_for_model(instance)
@@ -78,20 +80,20 @@ class RelatedObjectsDescriptor(object):
             }
         elif isinstance(instance, field.rel.to):
             return {field.name: instance}
-        
+
         raise TypeError('Unable to query %s with %s' % (field, instance))
-    
+
     def get_query_from(self, instance):
         return self.get_query_for_field(instance, self.from_field)
-    
+
     def get_query_to(self, instance):
         return self.get_query_for_field(instance, self.to_field)
-    
+
     def contribute_to_class(self, cls, name):
         self.name = name
         self.model_class = cls
         setattr(cls, self.name, self)
-    
+
     def __get__(self, instance, cls=None):
         if instance is None:
             return self
@@ -110,15 +112,20 @@ class RelatedObjectsDescriptor(object):
         return self.create_manager(instance,
                 self.related_model._base_manager.__class__)
 
-    def create_manager(self, instance, superclass):
+    def create_manager(self, instance, superclass, cf_from=True):
         rel_obj = self
-        core_filters = self.get_query_from(instance)
-        uses_gfk = self.is_gfk(self.to_field)
+        if cf_from:
+            core_filters = self.get_query_from(instance)
+            rel_field = self.to_field
+        else:
+            core_filters = self.get_query_to(instance)
+            rel_field = self.from_field
+        uses_gfk = self.is_gfk(rel_field)
 
         class RelatedManager(superclass):
             def get_query_set(self):
                 if uses_gfk:
-                    qs = GFKOptimizedQuerySet(self.model, gfk_field=rel_obj.to_field)
+                    qs = GFKOptimizedQuerySet(self.model, gfk_field=rel_field)
                     return qs.filter(**(core_filters))
                 else:
                     return superclass.get_query_set(self).filter(**(core_filters))
@@ -155,14 +162,15 @@ class RelatedObjectsDescriptor(object):
             def clear(self):
                 self.all().delete()
             clear.alters_data = True
-            
+
             def connect(self, obj, **kwargs):
                 kwargs.update(rel_obj.get_query_to(obj))
                 connection, created = self.get_or_create(**kwargs)
                 return connection
-            
+
             def related_to(self):
-                return rel_obj.related_model._default_manager.filter(
+                mgr = rel_obj.create_manager(instance, superclass, False)
+                return mgr.filter(
                     **rel_obj.get_query_to(instance)
                 )
 
@@ -171,7 +179,7 @@ class RelatedObjectsDescriptor(object):
         manager.model = self.related_model
 
         return manager
-    
+
     def all(self):
         if self.is_gfk(self.from_field):
             ctype = ContentType.objects.get_for_model(self.model_class)
@@ -195,10 +203,10 @@ class BaseGFKRelatedObject(models.Model):
     object_type = models.ForeignKey(ContentType, related_name="related_%(class)s")
     object_id = models.IntegerField(db_index=True)
     object = GenericForeignKey(ct_field="object_type", fk_field="object_id")
-    
+
     class Meta:
         abstract = True
-    
+
 
 class RelatedObject(BaseGFKRelatedObject):
     """
@@ -208,9 +216,9 @@ class RelatedObject(BaseGFKRelatedObject):
     """
     alias = models.CharField(max_length=255, blank=True)
     creation_date = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ('-creation_date',)
-    
+
     def __unicode__(self):
         return '%s related to %s ("%s")' % (self.parent, self.object, self.alias)
