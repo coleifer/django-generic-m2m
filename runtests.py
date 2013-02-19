dceae6725d10a5d1af6287e1b684c651683d1750
Charles Leifer <coleifer@gmail.com>
2013-02-18 19:12:30 -0600

    Allow tests to be run on 1.4

--- runtests.py
+++ runtests.py
@@ -2,21 +2,21 @@
 import sys
 from os.path import dirname, abspath
 
+import django
 from django.conf import settings
 
 
 if len(sys.argv) > 1 and 'postgres' in sys.argv:
     sys.argv.remove('postgres')
-    db_engine = 'postgresql_psycopg2'
+    db_engine = 'django.db.backends.postgresql_psycopg2'
     db_name = 'test_main'
 else:
-    db_engine = 'sqlite3'
+    db_engine = 'django.db.backends.sqlite3'
     db_name = ''
 
 if not settings.configured:
     settings.configure(
-        DATABASE_ENGINE = db_engine,
-        DATABASE_NAME = db_name,
+        DATABASES=dict(default=dict(ENGINE=db_engine, NAME=db_name)),
         INSTALLED_APPS = [
             'django.contrib.contenttypes',
             'genericm2m',
@@ -24,7 +24,7 @@ if not settings.configured:
         ],
     )
 
-from django.test.simple import run_tests
+from django.test.utils import get_runner
 
 
 def runtests(*test_args):
@@ -32,9 +32,10 @@ def runtests(*test_args):
         test_args = ['genericm2m_tests']
     parent = dirname(abspath(__file__))
     sys.path.insert(0, parent)
-    failures = run_tests(test_args, verbosity=1, interactive=True)
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True)
+    failures = test_runner.run_tests(test_args)
     sys.exit(failures)
-
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
