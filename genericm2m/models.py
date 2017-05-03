9a2d9ae7b8b47974379e36fca40b0c50fe3a0642
Eugene Morozov <eugene.morozov@gmail.com>
2017-05-04 00:35:35 +0300

    Implemented support for non-integer primary keys.

--- genericm2m/models.py
+++ genericm2m/models.py
@@ -226,12 +226,12 @@ class BaseGFKRelatedObject(models.Model):
     """
     # SOURCE OBJECT:
     parent_type = models.ForeignKey(ContentType, related_name="child_%(class)s")
-    parent_id = models.IntegerField(db_index=True)
+    parent_id = models.TextField(db_index=True)
     parent = GenericForeignKey(ct_field="parent_type", fk_field="parent_id")
 
     # ACTUAL RELATED OBJECT:
     object_type = models.ForeignKey(ContentType, related_name="related_%(class)s")
-    object_id = models.IntegerField(db_index=True)
+    object_id = models.TextField(db_index=True)
     object = GenericForeignKey(ct_field="object_type", fk_field="object_id")
 
     class Meta:
